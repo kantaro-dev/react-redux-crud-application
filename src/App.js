0dd720f7f979afa15fff3144f2baeac5588ef311
kantaro <kantarodev@gmail.com>
2022-04-22 09:21:21 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React, { Component } from "react";
 
+// class App extends Component {
+//   render() {
+//     return React.createElement("div", null, "Hello, world");
+//   }
+// }
+
+// class App extends Component {
+//   render() {
+//     const greeting = "Hi! React";
+//     const dom = <h1 className="foo">{greeting}</h1>;
+
+//     return dom;
+//   }
+// }
+
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello, world!</h1>
-      </div>
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input
+          type="text"
+          onChange={() => {
+            console.log("I am changed!");
+          }}
+        />
+      </React.Fragment>
     );
   }
 }
