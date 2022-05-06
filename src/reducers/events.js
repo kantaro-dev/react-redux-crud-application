4a92dcbbf19fadbc04273e731f4754a94f0ce6fb
kantaro <kantarodev@gmail.com>
2022-05-06 20:16:25 +0900

    implement events index

---
+++ src/reducers/events.js
@@ -0,0 +1,12 @@
+/* eslint-disable import/no-anonymous-default-export */
+import _ from "lodash";
+import { READ_EVENTS } from "../actions";
+
+export default (events = {}, action) => {
+  switch (action.type) {
+    case READ_EVENTS:
+      return _.mapKeys(action.response.data, "id");
+    default:
+      return events;
+  }
+};
